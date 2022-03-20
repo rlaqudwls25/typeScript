cde8bf2f628e30b79bf5fb88bb5ff93d7aac72a4
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-03-20 19:05:28 +0900

    다양한 타입 종류

--- src/1.ts
+++ src/1.ts
@@ -1,4 +1,26 @@
-import { isEqual } from "lodash";
+export {};
 
-const v1 = 123;
-const v2 = () => console.log("123");
+const value: number[] = [1, 2, 3];
+const value2: Array<number> = [1, 2, 3];
+
+// value.push("a");  value는 number만 푸쉬할 수 있다.
+// value.push(1)
+
+const data: [string, number] = ["msg", 1];
+
+// data[0].substr(1);
+// data[1].substr(1); number에는 substr 형식이 없다.
+
+let v1: undefined = undefined;
+let v2: null = null;
+let v3: undefined | number = undefined;
+
+// v2 = 123; null이기 때문에 값이 들어갈 수 없다.
+// v3 = 123;
+interface props {
+  name: string;
+}
+let a1: props = { name: "jin" }; // ojbect는 interface를 통하여 타입 지정 가능
+
+type Width = string | number;
+let width: Width = 100;
