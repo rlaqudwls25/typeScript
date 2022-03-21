fbb4ebe9964e412b03a66034e1d32eb5f6987bf8
rlaqudwls25 <rlaqudwls157@gmail.com>
2022-03-21 23:38:11 +0900

    제네릭 알아보기

--- src/1.ts
+++ src/1.ts
@@ -40,5 +40,36 @@ enum Language {
 
 // console.log(Fruit);
 // console.log(Fruit[0]);
-
 // console.log(Language);
+
+interface Person {
+  name: string;
+  age?: number;
+}
+
+interface Product {
+  name: string;
+  age: number;
+}
+
+const obj = {
+  name: "mike",
+};
+
+const person: Person = obj;
+// const product: Product = obj;
+
+// Person의 값의 집합이 Product의 값의 집합보다 크다
+
+// 제네릭
+const makeArray = <T>(defaultValue: T, size: T | number): T[] => {
+  const arr: T[] = [];
+  for (let i = 0; i < size; i++) {
+    arr.push(defaultValue);
+  }
+  return arr;
+};
+
+const arr1 = makeArray(1, 10);
+const arr2 = makeArray("1", 10);
+const arr3 = makeArray(true, "10");
